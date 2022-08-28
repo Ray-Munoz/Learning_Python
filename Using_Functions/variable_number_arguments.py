41e906558a537704c48fc6e3b7578419e69cea44
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-08-28 13:43:02 -0700

    Functions: Creating and calling the function

---
+++ Using_Functions/variable_number_arguments.py
@@ -0,0 +1,8 @@
+# Create a function that allows any number of given arguments
+
+# function of numbers
+def func1(*numbers):
+    print('{}' .format(numbers))
+
+# calling the function
+func1(1,2,5,7,10)
