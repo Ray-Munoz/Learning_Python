f44080c7ec6de1b5b9ea5471c05799ba2e8ee740
Raymundo Munoz <111741380+Ray-Munoz@users.noreply.github.com>
2022-08-28 12:05:36 -0700

    Add files via upload

---
+++ Name_Age_Function.py
@@ -0,0 +1,9 @@
+# Create a function with parameters "name","age"
+# Give it arguments: your name and your age
+
+def name_age(name, age):
+    print('Your name is: {}' .format(name))
+    print('You are {} years old' .format(age))
+    greet = "Hello {}" .format(name)
+
+name_age('Ray', 22)
