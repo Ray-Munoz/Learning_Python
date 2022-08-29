e17f65ee534e24ea94e39ba748869ad8a004bdc6
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-08-29 11:42:57 -0700

    Objects, vehicle_class: add name attribute

--- Using_Objects/vehicle_class.py
+++ Using_Objects/vehicle_class.py
@@ -1,9 +1,10 @@
 # Create a vehicle class with max_speed and mileage attributes
 
 class vehicle:
-    def __init__(self, max_speed, mileage):
+    def __init__(self, name, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
+        self.name = name
 
 # modelX example
 modelX = vehicle(240, 18)
