09f31e3b6c7744cf0787b352b3da7ec786bfc724
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-08-29 10:34:10 -0700

    Objects: Create vehicle class

--- Using_Objects/vehicle_class.py
+++ Using_Objects/vehicle_class.py
@@ -1,3 +1,8 @@
 # Create a vehicle class with max_speed and mileage attributes
 
+class vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
 
+        
