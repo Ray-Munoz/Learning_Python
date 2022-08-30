2132ac185ae827ea23e5d3f9ed4d609fa0f839f2
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-08-30 09:40:41 -0700

    Sets_lists, sets.py: union of list in set

--- Sets_Lists/sets.py
+++ Sets_Lists/sets.py
@@ -3,3 +3,6 @@
 # Set and list of colors
 list_colors = ['yellow','orange','black']
 set_colors = {'blue', 'green', 'red'}
+
+# Combining list into set of colors
+print(set_colors.union(list_colors))
