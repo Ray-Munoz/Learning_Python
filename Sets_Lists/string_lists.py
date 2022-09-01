7a7940f8e074db1bd2bb34fbba1fe67a869e92a5
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-08-31 22:20:59 -0700

    Sets_Lists, string_lists.py: palindrome or not palindrome

--- Sets_Lists/string_lists.py
+++ Sets_Lists/string_lists.py
@@ -1,4 +1,12 @@
 # Tells a user if the string given is a palindrome
 # palindrome: reads the same forward as it does backwards
 
+# input a word from user
 
+s = input("Please enter a word: ")
+a = s[::-1]
+b = s[0:]
+if a == b:
+    print("This is a palindrome!")
+else:
+    print("This is not a palindrome :(")
