7e3b98323239e55cce3a2dfd8deab1f7825a8b23
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-09-04 13:01:11 -0700

    Algorithms, powers_factorials: Creating the factorial function

--- Algorithms/power_factorials.py
+++ Algorithms/power_factorials.py
@@ -1,1 +1,10 @@
 # Create an algorithm that prints out the power of a number and the factorial of a number
+
+# The factorial function
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+       return num * factorial(num-1)
+
+print("{}! is {}. " .format(10 ,factorial(10)))
