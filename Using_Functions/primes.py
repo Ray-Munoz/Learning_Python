6f868cf8cdc814e8fa1de92a5ec69080beb91dff
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-09-04 20:32:43 -0700

    Using_functions, prime.py: Creating a divisor function

--- Using_Functions/primes.py
+++ Using_Functions/primes.py
@@ -1,3 +1,13 @@
 # Ask the user for a number and determine whether the number is prime or not.
 
 
+# Creating a divisor function
+def divisors(num):
+    list_of_num = range(1, num+1)
+    list_of_divisors = []
+    for n in list_of_num:
+        if num % n == 0:
+            list_of_divisors.append(n)
+    print(list_of_divisors)
+
+
