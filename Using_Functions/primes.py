0d0e3a360f16d764badefc9b341894be5b655081
Raymundo Munoz <ray.munozjr777@gmail.com>
2022-09-04 20:37:52 -0700

    Using_functions, prime.py: Determine if a number is a prime number

--- Using_Functions/primes.py
+++ Using_Functions/primes.py
@@ -8,6 +8,12 @@ def divisors(num):
     for n in list_of_num:
         if num % n == 0:
             list_of_divisors.append(n)
-    print(list_of_divisors)
+    return list_of_divisors
 
 
+# Determining if a number is prime
+num = int(input("Enter a number: "))
+if (len(divisors(num)) <= 2):
+    print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")
